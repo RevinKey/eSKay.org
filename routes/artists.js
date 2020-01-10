48ed00ede5a138f82799072282c703f5a29d7a27
Kevin Reynolds <kreynolds@brightcove.com>
2020-01-10 11:43:10 -0500

    lizaLevy

--- routes/artists.js
+++ routes/artists.js
@@ -1,59 +1,65 @@
 module.exports = {
-  artist: [
-    {
-      name: 'Shannon Reynolds',
-      title: 'Co-Founder/ Artistic Directors',
-      bio: 'is a dance educator and arts administrator who graduated from Russell Sage College with a BA in health science and dance and her MPA from Mercy College. Shannon is currently employed as the Company Manager  at STREB in Williamsburg, Brooklyn. Shannon has also taught dance to children and adults throughout the tri-state region. In 2016 Shannon has taken her goal of helping individuals gain access to all forms of art by creating eSKay Arts Collective with dancer and performer Kelly Flansburg. A choreographer, Shannon seeks to create movement which brings awareness and a safe environment difficult topics in hopes to begin conversations that are often left unspoken.',
-      links: '',
-      image: 'img/Shannon-1010.jpg'
-    }, {
-      name: 'Kelly Flansburg',
-      title: 'Co-Founder/ Artistic Directors',
-      bio: 'Kelly comes all the way from Albany,NY.  She was fortunate enough to attend the Complexions Contemporary Ballet Summer Intensive and Jacobs Pillow Contemporary Dance Program as well as an alumni of New York State Summer School of the Arts for Dance. Kelly graduated with a degree in Creative Arts and Therapy with a concentration in Dance from Russell Sage College in 2013. Kelly has been fortunate to work with numerous artists including Carolyn Adams, Francesca Harper, Danny Grossman, Erika Pujic and Robert Battle. Since coming to the city Kelly has tackled the freelance dancer life  performing with Bryce Dance Company, Jinah Parker’s Project:SHE as well as numerous performances with Earl Mosley’s Diversity of Dance. Kelly is Co-Director of eSKay Arts Collective, a collaboration of artists who are able to grow their ideas through sharing resources and engaging the talents and support of fellow artists in the collective. Kelly is beyond excited to start her new adventure with STREB!',
-      links: '',
-      image: 'img/IMG_2367.jpg'
-    }, {
-      name: 'Nadia Khayrallah',
-      title: 'Dancer/Choreographer',
-      bio: 'Nadia Khayrallah is a dancer, choreographer, filmmaker, and writer. Since graduating from Columbia University with a B.A. in Dance and Psychology, she has performed with Gotham Dance Theater, Trainor Dance, Artists by Any Other Name, SHIFT Dance Collective, eSKay Arts Collective, the Cunningham Trust, and the musical artist Roxiny. Nadia has presented her choreography through Sacred Circle Theater Collective, Artists by Any Other Name, HATCH Presenting Series, and Fertile Ground. She has written for The Dance Enthusiast, The Huffington Post, Sukoon Magazine, and Reductress. Nadia is Communications Coordinator for the Dance/NYC Junior Committee, a group of young professionals creating conversation on social and economic issues in the field. www.nadiak.tk ',
-      links: '',
-      image: 'img/NadiaKhayrallah.jpg'
-    }, {
-      name: 'Jennifer Jackson',
-      title: 'Dancer/Choreographer',
-      bio: 'Jennifer grew up in GA with extensive training in ballet, pointe, modern, jazz, and tap. She graduated with her BFA in Dance from Valdosta State University in 2013 with a focus in modern dance. She holds a P-12 Dance Education Certification and is the former dance director for Lamar Reese Magnet Elementary School of the Arts as well as KIPP Metro Atlanta Schools KWP. She has founded two school dance programs. Jennifer has worked and performed with a variety of professional dance companies in the Atlanta area including Xtensity Contemporary Dance Company, Room to Move Dance, Refuge Dance Company, Infinite Theatre Performing Company, MOTO Artis, and Withhart Dance Projects. Jennifer has taught and choreographed a variety of dance styles at dance studios throughout Georgia including Elite Atlanta Performers, Callanwolde Fine Arts Center, Footnotes Dance and Acrobatics, Georgia Stars Academy of Dance, Southwest Georgia Performing Arts, and Barefoot in Motion. She has participated in workshops and conventions from well-known choreographers and companies such as Tremaine, Ririe-Woodbury, Doug Caldwell, Zoetic Dance Ensemble summer sessions, Brooke Lipton, and more. Currently, Jennifer is teaching middle school dance in New York City. She is excited to be dancing and choreographing with eSKay Arts Collective.',
-      links: '',
-      image: 'img/JenniferJackson.jpg'
-    }, {
-      name: 'Laura Natario',
-      title: 'Dancer',
-      bio: 'began her formal dance training at The Massachusetts Academy of Ballet under the direction of Rose and Charles Flachs while she also attended the Pioneer Valley Performing Arts Charter Public High School, training under the direction of Jodi Falk. She received her bachelors of the fine arts degree from the University of Massachusetts, Amherst on a Chancellor’s Talent Scholarship. She recently completed a master of fine arts degree at Saint Mary’s College of California. She currently performs, teaches ballet and contemporary dance, and choreographs within the greater New York City area. She first performed with EsKay Artistic Collective as a 2018 summer guest artist. This is her first season as an EsKay company member.',
-      links: '',
-      image: 'img/lauraNatario.jpeg'
-    },{
-      name: 'Jen Silver',
-      title: 'Dancer',
-      bio: "received her Master's Degree in Dance Education with PK-12 Dance Teacher Certification (NJ CEAS) from the Graduate School of Education at Rutgers University. Prior to this, Jen graduated Summa Cum Laude with her B.F.A. in Dance Performance from Mason Gross School of the Arts, Rutgers University. Professionally, Jen has danced with Marked Dance Project, Humanistics Dance Company, eSKay Arts Collective, Kaley Pruitt Dance, and Erin Pryor Dance Theatre. She has also performed works by choreographers including Jodi Melnick, Joe Goode, Randy James, John Evans, Keith A. Thompson, and Kim Gibilisco and has studied under the instruction of Carolyn Dorfman, Tiffany Mills, David Parker, Roni Koresh, David Parsons, Claudia Lavista, and Omar Carrum. Jen is excited to perform in her first season with eSKay Arts Collective!",
-      links: '',
-      image: 'img/JenSilver.jpg'
-    }, {
-      name: 'Marissa Graham',
-      title: 'Dancer/Choreographer',
-      bio: 'graduated from George Mason University with a Bachelor of Fine Arts in dance. She received the Mason Achievement Award 2011-2015 and the Kevin Campbell Summer Scholarship. She has performed with local NYC companies including Impact Movement Collective, Jessica Gaynor Dance, Vissi Dance Theatre (2 international tours) esKay Arts Collective, and Bloodline Dance Theater. She is a Certified Exercise Fitness Specialist. She has taught all ages from 1-18 and is currently teaching for DMF Youth, a non-profit dedicated to serving the underprivileged children of Manhattan. She has choreographed for Mason Dance Company and Vissi Dance Theatre.',
-      links: '',
-      image: 'img/marissa.JPG'
-    }, {
-      name: 'Becca Mann',
-      title: 'Dancer/Choreographer',
-      bio: 'Originally from Philadelphia, Pennsylvania, Becca began her dance training under the direction of Scott Jovovich and Liz Fitchett Jovovich. She graduated from DeSales University in 2017 with a B.A in Dance and a Business minor. During her collegiate experience, she had the opportunity to perform pieces choreographed by artists such as Doris Humphrey (staging by Dr. Jessica Van Oort), Tara Madsen Robbins, Paul Ocampo, Jeff Shade, Jessica Chen, and Michio Ito (staging by Kumiko and Imura). Recently relocating to the NYC area, Becca could not be more excited to be a new member of eSKay Arts Collective.',
-      links: '',
-      image: 'img/Becca.jpg'
-    }, {
-      name: 'Danielle Mumpower',
-      title: 'Dancer/Choreographer',
-      bio: 'was last seen at the Southern Vermont Dance Festival in Brattleboro, VT with EsKay Arts Collective, in Jen Jackson\'s "Unchecked Baggage" and in Fall 2018 in Cara Harker\'s From the Sublime to the Ridiculous at the InFringe Festival in New Orleans, LA. She also premiered a new work as a choreographer entitled “Double Tap”, a cheeky take on addiction to technology. A resident of NYC for six years, she originally hails from Bristol, VA/TN and celebrated 26 years of dance training this year. She studied at ETSU with a B.A. In theatre and minor in dance, training under the incomparable Cara Harker pushing her to achieve the Judith Woodruff Award for Excellence in Dance. Since moving to NYC, Danielle has performed in venues throughout the city and East coast including a sold out run of Roadkill at IRT Theater with choreographers and creators Lauren Lasorda and Tali Custer, and the premier of Raiz with Silva Dance Company at the Martha Graham Theatre. She is a member of Improbable Stage, and House of the Roses, A Volunteer Dance Company. Most days you can find Danielle in the ballet studio, she is currently a lead teacher at Dance with Miss Rachel in Tribeca. @danielle_mumpower',
-      links: '',
-      image: 'img/DanielleMumpower.jpg'
-    }
-  ]
+    artist: [
+        {
+            name: 'Shannon Reynolds',
+            title: 'Co-Founder/ Artistic Directors',
+            bio: 'is a dance educator and arts administrator who graduated from Russell Sage College with a BA in health science and dance and her MPA from Mercy College. Shannon is currently employed as the Company Manager  at STREB in Williamsburg, Brooklyn. Shannon has also taught dance to children and adults throughout the tri-state region. In 2016 Shannon has taken her goal of helping individuals gain access to all forms of art by creating eSKay Arts Collective with dancer and performer Kelly Flansburg. A choreographer, Shannon seeks to create movement which brings awareness and a safe environment difficult topics in hopes to begin conversations that are often left unspoken.',
+            links: '',
+            image: 'img/Shannon-1010.jpg'
+        }, {
+            name: 'Kelly Flansburg',
+            title: 'Co-Founder/ Artistic Directors',
+            bio: 'Kelly comes all the way from Albany,NY.  She was fortunate enough to attend the Complexions Contemporary Ballet Summer Intensive and Jacobs Pillow Contemporary Dance Program as well as an alumni of New York State Summer School of the Arts for Dance. Kelly graduated with a degree in Creative Arts and Therapy with a concentration in Dance from Russell Sage College in 2013. Kelly has been fortunate to work with numerous artists including Carolyn Adams, Francesca Harper, Danny Grossman, Erika Pujic and Robert Battle. Since coming to the city Kelly has tackled the freelance dancer life  performing with Bryce Dance Company, Jinah Parker’s Project:SHE as well as numerous performances with Earl Mosley’s Diversity of Dance. Kelly is Co-Director of eSKay Arts Collective, a collaboration of artists who are able to grow their ideas through sharing resources and engaging the talents and support of fellow artists in the collective. Kelly is beyond excited to start her new adventure with STREB!',
+            links: '',
+            image: 'img/IMG_2367.jpg'
+        }, {
+            name: 'Danielle Mumpower',
+            title: 'Dancer/Choreographer',
+            bio: 'was last seen at the Southern Vermont Dance Festival in Brattleboro, VT with EsKay Arts Collective, in Jen Jackson\'s "Unchecked Baggage" and in Fall 2018 in Cara Harker\'s From the Sublime to the Ridiculous at the InFringe Festival in New Orleans, LA. She also premiered a new work as a choreographer entitled “Double Tap”, a cheeky take on addiction to technology. A resident of NYC for six years, she originally hails from Bristol, VA/TN and celebrated 26 years of dance training this year. She studied at ETSU with a B.A. In theatre and minor in dance, training under the incomparable Cara Harker pushing her to achieve the Judith Woodruff Award for Excellence in Dance. Since moving to NYC, Danielle has performed in venues throughout the city and East coast including a sold out run of Roadkill at IRT Theater with choreographers and creators Lauren Lasorda and Tali Custer, and the premier of Raiz with Silva Dance Company at the Martha Graham Theatre. She is a member of Improbable Stage, and House of the Roses, A Volunteer Dance Company. Most days you can find Danielle in the ballet studio, she is currently a lead teacher at Dance with Miss Rachel in Tribeca. @danielle_mumpower',
+            links: '',
+            image: 'img/DanielleMumpower.jpg'
+        }, {
+            name: 'Liza Marie Levy',
+            title: 'Dancer/Choreographer',
+            bio: 'Originally from Harleysville, PA, Liza Marie graduated cum laude with a B.A. in Dance from DeSales University. While there, she worked with choreographers such as Sean Sullivan, Colby Damon, Angela Sigley Grossman, Er-Dong Hu, Julia Mayo, and Trinette Singleton. She has also had the privilege of performing in a reconstruction of José Limón’s Concerto Grosso. Since moving to the city she has performed with BalaSole Dance Company, eSKay Arts Collective, Muniz and Makers Dance Company, and DoubleTake Dance. Liza is currently working as the Administrative Assistant for Mari Meade Dance Collective/MMDC and dancing with DoubleTake Dance and eSKay Arts Collective. Along with performing, Liza is also teaching dance and acrobatics at TamiCo Dancing and Fancy Feet Dance in the Bronx.',
+            links: '',
+            image: 'img/LizaLevy.jpg'
+        }, {
+            name: 'Nadia Khayrallah',
+            title: 'Dancer/Choreographer',
+            bio: 'Nadia Khayrallah is a dancer, choreographer, filmmaker, and writer. Since graduating from Columbia University with a B.A. in Dance and Psychology, she has performed with Gotham Dance Theater, Trainor Dance, Artists by Any Other Name, SHIFT Dance Collective, eSKay Arts Collective, the Cunningham Trust, and the musical artist Roxiny. Nadia has presented her choreography through Sacred Circle Theater Collective, Artists by Any Other Name, HATCH Presenting Series, and Fertile Ground. She has written for The Dance Enthusiast, The Huffington Post, Sukoon Magazine, and Reductress. Nadia is Communications Coordinator for the Dance/NYC Junior Committee, a group of young professionals creating conversation on social and economic issues in the field. www.nadiak.tk ',
+            links: '',
+            image: 'img/NadiaKhayrallah.jpg'
+        }, {
+            name: 'Jennifer Jackson',
+            title: 'Dancer/Choreographer',
+            bio: 'Jennifer grew up in GA with extensive training in ballet, pointe, modern, jazz, and tap. She graduated with her BFA in Dance from Valdosta State University in 2013 with a focus in modern dance. She holds a P-12 Dance Education Certification and is the former dance director for Lamar Reese Magnet Elementary School of the Arts as well as KIPP Metro Atlanta Schools KWP. She has founded two school dance programs. Jennifer has worked and performed with a variety of professional dance companies in the Atlanta area including Xtensity Contemporary Dance Company, Room to Move Dance, Refuge Dance Company, Infinite Theatre Performing Company, MOTO Artis, and Withhart Dance Projects. Jennifer has taught and choreographed a variety of dance styles at dance studios throughout Georgia including Elite Atlanta Performers, Callanwolde Fine Arts Center, Footnotes Dance and Acrobatics, Georgia Stars Academy of Dance, Southwest Georgia Performing Arts, and Barefoot in Motion. She has participated in workshops and conventions from well-known choreographers and companies such as Tremaine, Ririe-Woodbury, Doug Caldwell, Zoetic Dance Ensemble summer sessions, Brooke Lipton, and more. Currently, Jennifer is teaching middle school dance in New York City. She is excited to be dancing and choreographing with eSKay Arts Collective.',
+            links: '',
+            image: 'img/JenniferJackson.jpg'
+        }, {
+            name: 'Laura Natario',
+            title: 'Dancer',
+            bio: 'began her formal dance training at The Massachusetts Academy of Ballet under the direction of Rose and Charles Flachs while she also attended the Pioneer Valley Performing Arts Charter Public High School, training under the direction of Jodi Falk. She received her bachelors of the fine arts degree from the University of Massachusetts, Amherst on a Chancellor’s Talent Scholarship. She recently completed a master of fine arts degree at Saint Mary’s College of California. She currently performs, teaches ballet and contemporary dance, and choreographs within the greater New York City area. She first performed with EsKay Artistic Collective as a 2018 summer guest artist. This is her first season as an EsKay company member.',
+            links: '',
+            image: 'img/lauraNatario.jpeg'
+        }, {
+            name: 'Jen Silver',
+            title: 'Dancer',
+            bio: "received her Master's Degree in Dance Education with PK-12 Dance Teacher Certification (NJ CEAS) from the Graduate School of Education at Rutgers University. Prior to this, Jen graduated Summa Cum Laude with her B.F.A. in Dance Performance from Mason Gross School of the Arts, Rutgers University. Professionally, Jen has danced with Marked Dance Project, Humanistics Dance Company, eSKay Arts Collective, Kaley Pruitt Dance, and Erin Pryor Dance Theatre. She has also performed works by choreographers including Jodi Melnick, Joe Goode, Randy James, John Evans, Keith A. Thompson, and Kim Gibilisco and has studied under the instruction of Carolyn Dorfman, Tiffany Mills, David Parker, Roni Koresh, David Parsons, Claudia Lavista, and Omar Carrum. Jen is excited to perform in her first season with eSKay Arts Collective!",
+            links: '',
+            image: 'img/JenSilver.jpg'
+        }, {
+            name: 'Marissa Graham',
+            title: 'Dancer/Choreographer',
+            bio: 'graduated from George Mason University with a Bachelor of Fine Arts in dance. She received the Mason Achievement Award 2011-2015 and the Kevin Campbell Summer Scholarship. She has performed with local NYC companies including Impact Movement Collective, Jessica Gaynor Dance, Vissi Dance Theatre (2 international tours) esKay Arts Collective, and Bloodline Dance Theater. She is a Certified Exercise Fitness Specialist. She has taught all ages from 1-18 and is currently teaching for DMF Youth, a non-profit dedicated to serving the underprivileged children of Manhattan. She has choreographed for Mason Dance Company and Vissi Dance Theatre.',
+            links: '',
+            image: 'img/marissa.JPG'
+        }, {
+            name: 'Becca Mann',
+            title: 'Dancer/Choreographer',
+            bio: 'Originally from Philadelphia, Pennsylvania, Becca began her dance training under the direction of Scott Jovovich and Liz Fitchett Jovovich. She graduated from DeSales University in 2017 with a B.A in Dance and a Business minor. During her collegiate experience, she had the opportunity to perform pieces choreographed by artists such as Doris Humphrey (staging by Dr. Jessica Van Oort), Tara Madsen Robbins, Paul Ocampo, Jeff Shade, Jessica Chen, and Michio Ito (staging by Kumiko and Imura). Recently relocating to the NYC area, Becca could not be more excited to be a new member of eSKay Arts Collective.',
+            links: '',
+            image: 'img/Becca.jpg'
+        }
+    ]
 };
